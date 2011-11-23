39f83dce2c0ed01f06dec45a5c048c655cf3f7c2
Guy Rozendorn <guy@rzn.co.il>
2011-11-23 14:20:46 +0200

    tests and fixes

--- src/infi/mountoolinux/mount/manager/__init__.py
+++ src/infi/mountoolinux/mount/manager/__init__.py
@@ -1,9 +1,12 @@
 
-from .show_mounts import MountRepositoryMixin
+from .show_mounts import MountRepositoryMixin, MountEntry
 from .supported_file_systems import SupportedFileSystemsMixin
+from .mounter import MounterMixin
 
 class MountManagerBaseClass(object):
     pass
 
-class MountManager(MountManagerBaseClass, MountRepositoryMixin, SupportedFileSystemsMixin):
+class MountManager(MountManagerBaseClass, MountRepositoryMixin, SupportedFileSystemsMixin, MounterMixin):
     pass
+
+__all__ = ["MountManager", "MountEntry", ]
